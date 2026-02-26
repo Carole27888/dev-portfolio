7b84397cefda7bbd215c632f08a6d540f201d536
Carole Mutemi <cmutemi27@gmail.com>
2026-02-26 09:31:35 +0300

    Added Download my CV section

--- src/components/HeroSection/HeroText.jsx
+++ src/components/HeroSection/HeroText.jsx
@@ -43,7 +43,7 @@ const HeroText = () => {
         className="mt-6"
       >
         <a
-          href="public/images/CAROLINEMUTEMI11CV(1).pdf"
+          href="/images/Carolinemutemicv14(1).pdf"
           download
           className="inline-block mt-4 px-6 py-3 bg-red text-white rounded-md hover:bg-orange-dark transition duration-300"
         >
